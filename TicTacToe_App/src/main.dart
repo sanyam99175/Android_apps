18092ae3f01207219c406e9b98ce4e900050e80c
sanyam99175 <45010805+sanyam99175@users.noreply.github.com>
2020-09-13 20:56:38 +0530

    Update main.dart

--- TicTacToe_App/src/main.dart
+++ TicTacToe_App/src/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+void main()=> runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Github App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(title: 'Parse JSON data'),
 
+    );
+  }
+}
